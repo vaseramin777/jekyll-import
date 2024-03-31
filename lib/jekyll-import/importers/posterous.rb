5ca94ffa20953e1ded258206f7a41c9b219cc42b
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:38:11 +0300

    improve

--- lib/jekyll-import/importers/posterous.rb
+++ lib/jekyll-import/importers/posterous.rb
@@ -3,10 +3,10 @@
 module JekyllImport
   module Importers
     class Posterous < Importer
-      def self.specify_options(c)
-        c.option "email",     "--email EMAIL", "Posterous email address"
-        c.option "password",  "--password PW", "Posterous password"
-        c.option "api_token", "--token TOKEN", "Posterous API Token"
+      def self.specify_options(config)
+        config.option "email",     "--email EMAIL", "Posterous email address"
+        config.option "password",  "--password PW", "Posterous password"
+        config.option "api_token", "--token TOKEN", "Posterous API Token"
       end
 
       def self.require_deps
@@ -21,34 +21,42 @@ module JekyllImport
       end
 
       def self.fetch(uri_str, limit = 10)
-        # You should choose better exception.
-        raise ArgumentError, "Stuck in a redirect loop. Please double check your email and password" if limit.zero?
+        return nil if limit.zero?
 
         response = nil
         Net::HTTP.start("posterous.com") do |http|
           req = Net::HTTP::Get.new(uri_str)
-          req.basic_auth @email, @pass
+          req.basic_auth email, pass
           response = http.request(req)
         end
 
         case response
-        when Net::HTTPSuccess     then response
-        when Net::HTTPRedirection then fetch(response["location"], limit - 1)
-        else response.error!
+        when Net::HTTPSuccess
+          response
+        when Net::HTTPRedirection
+          fetch(response["location"], limit - 1)
+        else
+          response.error!
         end
+      rescue StandardError => e
+        raise ArgumentError, "Failed to fetch data from Posterous API: #{e.message}"
       end
 
       def self.fetch_images(directory, imgs)
         def self.fetch_one(url, limit = 10)
-          raise ArgumentError, "HTTP redirect too deep" if limit.zero?
+          return nil if limit.zero?
 
-          response = Net::HTTP.get_response(URI.parse(url))
+          response = Net::HTTP.get(URI.parse(url))
           case response
-          when Net::HTTPSuccess     then response.body
-          when Net::HTTPRedirection then fetch_one(response["location"], limit - 1)
+          when Net::HTTPSuccess
+            response
+          when Net::HTTPRedirection
+            fetch_one(response["location"], limit - 1)
           else
             response.error!
           end
+        rescue StandardError => e
+          raise ArgumentError, "Failed to fetch image: #{e.message}"
         end
 
         FileUtils.mkdir_p directory
@@ -65,21 +73,25 @@ module JekyllImport
         end
 
         urls
+      rescue StandardError => e
+        raise ArgumentError, "Failed to fetch and save images: #{e.message}"
       end
 
       def self.process(options)
-        email     = options.fetch("email")
-        pass      = options.fetch("password")
+        email = options.fetch("email")
+        pass = options.fetch("password")
         api_token = options.fetch("api_token")
 
-        @email = email
-        @pass = pass
-        @api_token = api_token
-        defaults = { :include_imgs => false, :blog => "primary", :base_path => "/" }
-        opts = defaults.merge(opts)
+        defaults = { include_imgs: false, blog: "primary", base_path: "/" }
+        opts = defaults.merge(options)
         FileUtils.mkdir_p "_posts"
 
-        posts = JSON.parse(fetch("/api/v2/users/me/sites/#{opts[:blog]}/posts?api_token=#{@api_token}").body)
+        begin
+          posts = JSON.parse(fetch("/api/v2/users/me/sites/#{opts[:blog]}/posts?api_token=#{api_token}").body)
+        rescue JSON::ParserError
+          raise ArgumentError, "Failed to parse JSON data from Posterous API"
+        end
+
         page = 1
 
         while posts.any?
@@ -92,7 +104,6 @@ module JekyllImport
             basename = format("%02d-%02d-%02d-%s", date.year, date.month, date.day, slug)
             name = basename + ".html"
 
-            # Images:
             if opts[:include_imgs]
               post_imgs = post["media"]["images"]
               if post_imgs.any?
@@ -100,24 +111,20 @@ module JekyllImport
                 img_urls = fetch_images(img_dir, post_imgs)
 
                 img_urls.map! do |url|
-                  '<li><img src="' + opts[:base_path] + url + '"></li>'
+                  "<li><img src='#{opts[:base_path]}#{url}'></li>"
                 end
                 imgcontent = "<ol>\n" + img_urls.join("\n") + "</ol>\n"
 
-                # filter out "posterous-content", replacing with imgs:
                 content = content.sub(%r!\<p\>\[\[posterous-content:[^\]]+\]\]\<\/p\>!, imgcontent)
               end
             end
 
-            # Get the relevant fields as a hash, delete empty fields and convert
-            # to YAML for the header
             data = {
-              "layout"    => "post",
-              "title"     => title.to_s,
-              "published" => published,
+              layout: "post",
+              title: title.to_s,
+              published: published,
             }.delete_if { |_k, v| v.nil? || v == "" }.to_yaml
 
-            # Write out the data and content to file
             File.open("_posts/#{name}", "w") do |f|
               f.puts data
               f.puts "---"
@@ -126,8 +133,14 @@ module JekyllImport
           end
 
           page += 1
-          posts = JSON.parse(fetch("/api/v2/users/me/sites/#{opts[:blog]}/posts?api_token=#{@api_token}&page=#{page}").body)
+          begin
+            posts = JSON.parse(fetch("/api/v2/users/me/sites/#{opts[:blog]}/posts?api_token=#{api_token}&page=#{page}").body)
+          rescue JSON::ParserError
+            raise ArgumentError, "Failed to parse JSON data from Posterous API"
+          end
         end
+      rescue StandardError => e
+        raise ArgumentError, "Failed to process Posterous data: #{e.message}"
       end
     end
   end
