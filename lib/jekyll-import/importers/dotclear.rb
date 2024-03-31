01a585788f25ad372a7876e9e651ab1dfa21222b
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:27 +0300

    improve

--- lib/jekyll-import/importers/dotclear.rb
+++ lib/jekyll-import/importers/dotclear.rb
@@ -14,15 +14,16 @@ module JekyllImport
         end
 
         def validate(opts)
-          file_path = opts["datafile"]
-          log_undefined_flag_error("datafile") if file_path.nil? || file_path.empty?
+          validate_flags(opts)
 
-          file_path = File.expand_path(file_path)
-          if File.open(file_path, "rb", &:readline).start_with?("///DOTCLEAR|")
+          file_path = opts["datafile"]
+          return unless File.exist?(file_path)
+
+          begin
             @data = read_export(file_path)
             Jekyll.logger.info "Export File:", file_path
-          else
-            Jekyll.logger.abort_with "Import Error:", "#{file_path.inspect} is not a valid Dotclear export file!"
+          rescue StandardError => e
+            Jekyll.logger.abort_with "Import Error:", "Failed to read export file: #{e.message}"
           end
 
           assets = @data["media"]
@@ -31,10 +32,9 @@ module JekyllImport
           Jekyll.logger.info "", "Media files detected in export data."
 
           media_dir = opts["mediafolder"]
-          log_undefined_flag_error("mediafolder") if media_dir.nil? || media_dir.empty?
+          return unless File.exist?(media_dir) && !File.empty?(media_dir)
 
-          media_dir = File.expand_path(media_dir)
-          log_invalid_media_dir_error(media_dir) if !File.directory?(media_dir) || Dir.empty?(media_dir)
+          Jekyll.logger.info "", "Media folder is valid."
         end
 
         def process(opts)
@@ -45,18 +45,16 @@ module JekyllImport
 
         private
 
-        # Parse backup sections into a Hash of arrays.
-        #
-        # Each section is of following shape:
-        #
-        #   [key alpha,beta,gamma,...]
-        #   lorem,ipsum,dolor,...
-        #   red,blue,green,...
-        #
-        # Returns Hash of shape:
-        #
-        #   {key => [{alpha => lorem,...}, {alpha => red,...}]}
-        #
+        def validate_flags(opts)
+          [["datafile"], ["mediafolder"]].each do |flag|
+            log_undefined_flag_error(flag.first) if opts[flag.first].nil? || opts[flag.first].empty?
+          end
+        end
+
+        def log_undefined_flag_error(label)
+          Jekyll.logger.abort_with "Import Error:", "--#{label} flag cannot be undefined, null or empty!"
+        end
+
         def read_export(file)
           ignored_sections = %w(category comment link setting)
 
@@ -67,7 +65,7 @@ module JekyllImport
             headers = header.split(",")
 
             data[key] = rows.each_line.with_object([]) do |line, bucket|
-              bucket << headers.zip(sanitize_line!(line)).to_h
+              bucket << Hash[headers.zip(line.split('","'))]
             end
 
             data
@@ -84,43 +82,9 @@ module JekyllImport
           end
         end
 
-        def log_undefined_flag_error(label)
-          Jekyll.logger.abort_with "Import Error:", "--#{label} flag cannot be undefined, null or empty!"
-        end
-
-        def log_invalid_media_dir_error(media_dir)
-          Jekyll.logger.error "Import Error:", "--mediafolder should be a non-empty directory."
-          Jekyll.logger.abort_with "", "Please check #{media_dir.inspect}."
-        end
-
         def sanitize_line!(line)
           line.strip!
-          line.split('","').tap do |items|
-            items[0].delete_prefix!('"')
-            items[-1].delete_suffix!('"')
-          end
-        end
-
-        # -
-
-        REPLACE_MAP = {
-          '\"'                => '"',
-          '\r\n'              => "\n",
-          '\n'                => "\n",
-          "/dotclear/public/" => "/assets/dotclear/",
-          "/public/"          => "/assets/dotclear/",
-        }.freeze
-
-        REPLACE_RE = Regexp.union(REPLACE_MAP.keys)
-
-        private_constant :REPLACE_MAP, :REPLACE_RE
-
-        # -
-
-        def adjust_post_contents!(content)
-          content.strip!
-          content.gsub!(REPLACE_RE, REPLACE_MAP)
-          content
+          line.split('","').map! { |item| item.delete_prefix('"').delete_suffix('"') }
         end
 
         def import_posts
@@ -134,14 +98,10 @@ module JekyllImport
             date, title = post.values_at("post_creadt", "post_title")
             path = File.join("_drafts", Date.parse(date).strftime("%Y-%m-%d-") + Jekyll::Utils.slugify(title) + ".html")
 
-            excerpt = adjust_post_contents!(post["post_excerpt_xhtml"].to_s)
+            excerpt = import_post_content(post["post_excerpt_xhtml"])
             excerpt = nil if excerpt.empty?
 
-            # Unlike the paradigm in Jekyll-generated HTML, `post_content_xhtml` in the export data
-            # doesn't begin with `post_excerpt_xhtml`.
-            # Instead of checking whether the excerpt content exists elsewhere in the exported content
-            # string, always prepend excerpt onto content with an empty line in between.
-            content = [excerpt, post["post_content_xhtml"]].tap(&:compact!).join("\n\n")
+            content = [excerpt, import_post_content(post["post_content_xhtml"])].tap(&:compact!).join("\n\n")
 
             front_matter_data = {
               "layout"       => "post",
@@ -149,13 +109,21 @@ module JekyllImport
               "date"         => date,
               "lang"         => post["post_lang"],
               "tags"         => tags[post["post_id"]],
-              "original_url" => post["post_url"], # URL as included in the export-file.
+              "original_url" => post["post_url"],
               "excerpt"      => excerpt,
             }.tap(&:compact!)
 
             Jekyll.logger.info "Creating:", path
-            File.write(path, "#{YAML.dump(front_matter_data)}---\n\n#{adjust_post_contents!(content)}\n")
+            File.write(path, "#{YAML.dump(front_matter_data)}---\n\n#{content}\n")
           end
+        end
+
+        def import_post_content(content)
+          return "" if content.nil?
+
+          content.strip!
+          content.gsub!(REPLACE_RE, REPLACE_MAP)
+          content
         end
 
         def import_assets(src_dir)
@@ -165,19 +133,11 @@ module JekyllImport
 
           assets.each do |asset|
             file_path = File.join(src_dir, asset["media_file"])
-            if File.exist?(file_path)
-              dest_path = File.join("assets/dotclear", asset["media_file"])
-              FileUtils.mkdir_p(File.dirname(dest_path))
+            next if !File.exist?(file_path)
 
-              Jekyll.logger.info "Copying:", file_path
-              Jekyll.logger.info "To:", dest_path
-              FileUtils.cp_r file_path, dest_path
-            else
-              Jekyll.logger.info "Not found:", file_path
-            end
-          end
-        end
-      end
-    end
-  end
-end
+            dest_path = File.join("assets/dotclear", asset["media_file"])
+            FileUtils.mkdir_p(File.dirname(dest_path))
+
+            Jekyll.logger.info "Copying:", file_path
+            Jekyll.logger.info "To:", dest_path
+            FileUtils.cp_r
