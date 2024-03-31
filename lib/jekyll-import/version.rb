d40617800076e6ddc898288fdf342acd63539386
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:15 +0300

    improve

--- lib/jekyll-import/version.rb
+++ lib/jekyll-import/version.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 module JekyllImport
+  # Constants should be in UPPER_SNAKE_CASE
   VERSION = "0.24.0"
+
+  # Add a comment describing what this module does
+  module_function
+
+  # Version number should be a method, not a constant
+  def version
+    VERSION
+  end
 end
+
