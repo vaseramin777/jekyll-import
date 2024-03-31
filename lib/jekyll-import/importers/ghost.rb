2a91b5c94529166f6d15168c0d7829488bbeeb9a
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:21 +0300

    improve

--- lib/jekyll-import/importers/ghost.rb
+++ lib/jekyll-import/importers/ghost.rb
@@ -19,66 +19,70 @@ module JekyllImport
 
       def self.process(options)
         posts = fetch_posts(options.fetch("dbfile", "ghost.db"))
-        unless posts.empty?
-          FileUtils.mkdir_p("_posts")
-          FileUtils.mkdir_p("_drafts")
-          posts.each do |post|
-            write_post_to_file(post)
-          end
+        if posts.empty?
+          puts "No posts found in the database."
+        else
+          create_directories
+          write_posts_to_files(posts)
         end
       end
 
-      class << self
-        private
+      private
 
-        def fetch_posts(dbfile)
-          db = Sequel.sqlite(dbfile)
-          query = "SELECT `title`, `slug`, `markdown`, `created_at`, `published_at`, `status`, `page` FROM posts"
-          db[query]
+      def self.fetch_posts(dbfile)
+        db = Sequel.sqlite(dbfile)
+        query = "SELECT `title`, `slug`, `markdown`, `created_at`, `published_at`, `status`, `page` FROM posts"
+        db[query]
+      rescue Sequel::DatabaseConnectionError
+        puts "Error: Invalid database file."
+        []
+      end
+
+      def self.create_directories
+        %w(_posts _drafts).each { |dir| FileUtils.mkdir_p(dir) }
+      end
+
+      def self.get_layout(post)
+        post[:page] ? "page" : "post"
+      end
+
+      def self.write_posts_to_files(posts)
+        posts.each do |post|
+          filename, frontmatter, content = generate_file_data(post)
+          write_file(filename, frontmatter, content)
+        end
+      end
+
+      def self.generate_file_data(post)
+        draft = post[:status] == "draft"
+        date = Time.at(post[draft ? :created_at : :published_at].to_i / 1000)
+
+        layout = get_layout(post)
+
+        if post[:page]
+          filename = "#{post[:slug]}.markdown"
+        else
+          directory = draft ? "_drafts" : "_posts"
+          filename = File.join(directory, "#{date.strftime("%Y-%m-%d")}-#{post[:slug]}.markdown")
         end
 
-        def write_post_to_file(post)
-          # detect if the post is a draft
-          draft = post[:status].eql?("draft")
+        frontmatter = {
+          "layout" => layout,
+          "title" => post[:title],
+        }
 
-          # detect if the post is considered a static page
-          page = post[:page]
+        frontmatter["date"] = date unless post[:page] && draft
+        frontmatter["published"] = false if post[:page] && draft
+        frontmatter.delete_if { |_k, v| v.nil? || v == "" }
 
-          # the publish date if the post has been published, creation date otherwise
-          # The database stores timestamps in milliseconds, so we need to divide by 1000
-          # to get time in seconds.
-          date = Time.at(post[draft ? :created_at : :published_at].to_i / 1000)
+        [filename, frontmatter.to_yaml, post[:markdown]]
+      end
 
-          if page
-            # the filename under which the page is stored
-            filename = "#{post[:slug]}.markdown"
-          else
-            # the directory where the file will be saved to. either _drafts or _posts
-            directory = draft ? "_drafts" : "_posts"
-
-            # the filename under which the post is stored
-            filename = File.join(directory, "#{date.strftime("%Y-%m-%d")}-#{post[:slug]}.markdown")
-          end
-
-          # the YAML FrontMatter
-          frontmatter = {
-            "layout" => page ? "page" : "post",
-            "title"  => post[:title],
-          }
-          frontmatter["date"] = date if !page && !draft # only add the date to the frontmatter when the post is published
-          frontmatter["published"] = false if page && draft # set published to false for draft pages
-          frontmatter.delete_if { |_k, v| v.nil? || v == "" } # removes empty fields
-
-          # write the posts to disk
-          write_file(filename, frontmatter.to_yaml, post[:markdown])
-        end
-
-        def write_file(filename, frontmatter, content)
-          File.open(filename, "w") do |f|
-            f.puts frontmatter
-            f.puts "---"
-            f.puts content
-          end
+      def self.write_file(filename, frontmatter, content)
+        File.open(filename, "w") do |f|
+          f.puts frontmatter
+          f.puts "---"
+          f.puts content
         end
       end
     end
