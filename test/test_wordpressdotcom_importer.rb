2892de87308bebb3d44c6364ceb0f54d26acf2a9
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:33 +0300

    improve

--- test/test_wordpressdotcom_importer.rb
+++ test/test_wordpressdotcom_importer.rb
@@ -1,71 +1,81 @@
 require "helper"
+require "minitest/autorun"
 
 Importers::WordpressDotCom.require_deps
 
-class TestWordpressDotComMigrator < Test::Unit::TestCase
-  should "clean slashes from slugs" do
+class TestWordpressDotComMigrator < Minitest::Test
+  def test_clean_slashes_from_slugs
     test_title = "blogs part 1/2"
     assert_equal("blogs-part-1-2", Importers::WordpressDotCom.sluggify(test_title))
   end
 end
 
-class TestWordpressDotComItem < Test::Unit::TestCase
-  should "extract an item's title" do
-    node = Hpricot('
+class TestWordpressDotComItem < Minitest::Test
+  def setup
+    @node = Hpricot::XML('
       <item>
         <title>Dear Science</title>
       </item>').at("item")
 
-    item = Importers::WordpressDotCom::Item.new(node)
-    assert_equal("Dear Science", item.title)
+    @item = Importers::WordpressDotCom::Item.new(@node)
   end
 
-  should "use post_name for the permalink_title if it's there" do
-    node = Hpricot('
+  def test_extract_an_item_s_title
+    assert_equal("Dear Science", @item.title)
+  end
+
+  def test_use_post_name_for_the_permalink_title_if_it_s_there
+    @node = Hpricot::XML('
       <item>
         <wp:post_name>cookie-mountain</wp:post_name>
         <title>Dear Science</title>
       </item>').at("item")
 
-    item = Importers::WordpressDotCom::Item.new(node)
-    assert_equal("cookie-mountain", item.permalink_title)
+    @item = Importers::WordpressDotCom::Item.new(@node)
+    assert_permalink_title("cookie-mountain")
   end
 
-  should "sluggify title for the permalink_title if post_name is empty" do
-    node = Hpricot('
+  def test_sluggify_title_for_the_permalink_title_if_post_name_is_empty
+    @node = Hpricot::XML('
       <item>
         <wp:post_name></wp:post_name>
         <title>Dear Science</title>
       </item>').at("item")
 
-    item = Importers::WordpressDotCom::Item.new(node)
-    assert_equal("dear-science", item.permalink_title)
+    @item = Importers::WordpressDotCom::Item.new(@node)
+    assert_permalink_title("dear-science")
   end
 
-  should "return nil for the excerpt, if it's missing" do
-    node = Hpricot('
+  def test_return_nil_for_the_excerpt_if_it_s_missing
+    @node = Hpricot::XML('
       <item>
         <excerpt:encoded><![CDATA[]]></excerpt:encoded>
       </item>').at("item")
 
-    item = Importers::WordpressDotCom::Item.new(node)
-    assert_equal(nil, item.excerpt)
+    @item = Importers::WordpressDotCom::Item.new(@node)
+    assert_nil(@item.excerpt)
   end
 
-  should "extract the excerpt as plaintext, if it's present" do
-    node = Hpricot('
+  def test_extract_the_excerpt_as_plaintext_if_it_s_present
+    @node = Hpricot::XML('
       <item>
         <excerpt:encoded><![CDATA[...this one weird trick.]]></excerpt:encoded>
       </item>').at("item")
 
-    item = Importers::WordpressDotCom::Item.new(node)
-    assert_equal("...this one weird trick.", item.excerpt)
+    @item = Importers::WordpressDotCom::Item.new(@node)
+    assert_equal("...this one weird trick.", @item.excerpt)
+  end
+
+  private
+
+  def assert_permalink_title(expected)
+    assert_equal(expected, @item.permalink_title)
   end
 end
 
 class TestWordpressDotComPublishedItem < TestWordpressDotComItem
-  def node
-    Hpricot('
+  def setup
+    @node = Hpricot::XML('
       <item>
         <title>PostTitle</title>
         <link>https://www.example.com/post/123/post-title/</link>
@@ -74,69 +84,50 @@ class TestWordpressDotComPublishedItem < TestWordpressDotComItem
         <wp:status>publish</wp:status>
         <wp:post_date>2015-01-23 08:53:47</wp:post_date>
       </item>').at("item")
+
+    @item = Importers::WordpressDotCom::Item.new(@node)
   end
 
-  def item
-    Importers::WordpressDotCom::Item.new(node)
+  def test_extract_the_date_time_the_item_was_published
+    assert_equal(Time.new(2015, 1, 23, 8, 53, 47), @item.published_at)
   end
 
-  should "extract the date-time the item was published" do
-    assert_equal(Time.new(2015, 1, 23, 8, 53, 47), item.published_at)
+  def test_put_the_date_in_the_file_name
+    assert_file_name("2015-01-23-post-name.html")
   end
 
-  should "put the date in the file_name" do
-    assert_equal("2015-01-23-post-name.html", item.file_name)
+  def test_put_the_file_in_./_posts
+    assert_directory_name("_posts")
   end
 
-  should "put the file in ./_posts" do
-    assert_equal("_posts", item.directory_name)
+  def test_know_its_status
+    assert_equal("publish", @item.status)
   end
 
-  should "know its status" do
-    assert_equal("publish", item.status)
+  def test_be_published
+    assert_published
   end
 
-  should "be published" do
-    assert_equal(true, item.published?)
+  def test_extract_the_link_as_a_permalink
+    assert_equal("/post/123/post-title/", @item.permalink)
   end
 
-  should "extract the link as a permalink" do
-    assert_equal("/post/123/post-title/", item.permalink)
+  private
+
+  def assert_file_name(expected)
+    assert_equal(expected, @item.file_name)
+  end
+
+  def assert_directory_name(expected)
+    assert_equal(expected, @item.directory_name)
   end
 end
 
 class TestWordpressDotComDraftItem < TestWordpressDotComItem
-  def node
-    Hpricot('
+  def setup
+    @node = Hpricot::XML('
       <item>
         <wp:post_name>post-name</wp:post_name>
         <wp:post_type>post</wp:post_type>
         <wp:status>draft</wp:status>
-        <wp:post_date>0000-00-00 00:00:00</wp:post_date>
-      </item>').at("item")
-  end
-
-  def item
-    Importers::WordpressDotCom::Item.new(node)
-  end
-
-  should "extract a nil publish-date" do
-    assert_equal(nil, item.published_at)
-  end
-
-  should "not put the date in the file_name" do
-    assert_equal("post-name.html", item.file_name)
-  end
-
-  should "put the file in ./_drafts" do
-    assert_equal("_drafts", item.directory_name)
-  end
-
-  should "know its status" do
-    assert_equal("draft", item.status)
-  end
-
-  should "not be published" do
-    assert_equal(false, item.published?)
-  end
-end
+        <wp:
