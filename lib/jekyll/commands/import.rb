d5e61f5f2fdb8d4fb3fa1db434cd6350ff0fe343
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:51 +0300

    improve

--- lib/jekyll/commands/import.rb
+++ lib/jekyll/commands/import.rb
@@ -3,29 +3,30 @@
 module Jekyll
   module Commands
     class Import < Command
+      # Mapping of migrator names to their corresponding classes
       IMPORTERS = {
-        :blogger         => "Blogger",
-        :behance         => "Behance",
-        :csv             => "CSV",
-        :drupal6         => "Drupal6",
-        :drupal7         => "Drupal7",
-        :enki            => "Enki",
-        :joomla          => "Joomla",
-        :joomla3         => "Joomla3",
-        :jrnl            => "Jrnl",
-        :ghost           => "Ghost",
-        :google_reader   => "GoogleReader",
-        :marley          => "Marley",
-        :mephisto        => "Mephisto",
-        :mt              => "MT",
-        :posterous       => "Posterous",
-        :rss             => "RSS",
-        :s9y             => "S9Y",
-        :textpattern     => "TextPattern",
-        :tumblr          => "Tumblr",
-        :typo            => "Typo",
-        :wordpress       => "WordPress",
-        :wordpressdotcom => "WordpressDotCom",
+        blogger:         "Blogger",
+        behance:         "Behance",
+        csv:             "CSV",
+        drupal6:         "Drupal6",
+        drupal7:         "Drupal7",
+        enki:            "Enki",
+        joomla:          "Joomla",
+        joomla3:         "Joomla3",
+        jrnl:            "Jrnl",
+        ghost:           "Ghost",
+        google_reader:   "GoogleReader",
+        marley:          "Marley",
+        mephisto:        "Mephisto",
+        mt:              "MT",
+        posterous:       "Posterous",
+        rss:             "RSS",
+        s9y:             "S9Y",
+        textpattern:     "TextPattern",
+        tumblr:          "Tumblr",
+        typo:            "Typo",
+        wordpress:       "WordPress",
+        wordpressdotcom: "WordpressDotCom",
       }.freeze
 
       class << self
@@ -37,9 +38,9 @@ module Jekyll
 
             c.action do |args, _options|
               if args.empty?
-                Jekyll.logger.warn "You must specify an importer."
-                Jekyll.logger.info "Valid options are:"
-                importers.sort.each { |i| Jekyll.logger.info "*", i.to_s }
+                puts "You must specify an importer."
+                puts "Valid options are:"
+                IMPORTERS.each_key { |i| puts "* #{i}" }
               end
             end
           end
@@ -49,13 +50,15 @@ module Jekyll
           migrator = migrator.to_s.downcase
 
           if IMPORTERS.key?(migrator.to_sym)
-            if JekyllImport::Importers.const_defined?(IMPORTERS[migrator.to_sym])
-              klass = JekyllImport::Importers.const_get(IMPORTERS[migrator.to_sym])
+            begin
+              klass = const_get(IMPORTERS[migrator.to_sym])
               if options.respond_to?(:__hash__)
                 klass.run(options.__hash__)
               else
                 klass.run(options)
               end
+            rescue NameError
+              abort_on_invalid_migrator(migrator)
             end
           else
             abort_on_invalid_migrator(migrator)
@@ -63,9 +66,9 @@ module Jekyll
         end
 
         def abort_on_invalid_migrator(migrator)
-          warn "Sorry, '#{migrator}' isn't a valid migrator. Valid choices:"
-          IMPORTERS.each_key { |k| warn "* #{k}" }
-          raise "'#{migrator}' is not a valid migrator."
+          puts "Sorry, '#{migrator}' isn't a valid migrator. Valid choices:"
+          IMPORTERS.each_key { |k| puts "* #{k}" }
+          abort "'#{migrator}' is not a valid migrator."
         end
       end
     end
