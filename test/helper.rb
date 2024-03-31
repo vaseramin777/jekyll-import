6b2c3d705875af95564d9bb3f163a9f790697063
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:32 +0300

    improve

--- test/helper.rb
+++ test/helper.rb
@@ -1,56 +1,45 @@
-if ENV["COVERAGE"] == "true"
-  require "simplecov"
-  require "simplecov-gem-adapter"
-  SimpleCov.start("gem")
+# .ruby-version
+2.7.2
+
+# .gitignore
+/log
+/tmp
+
+# Gemfile
+source 'https://rubygems.org'
+
+gem 'jekyll', '~> 4.2'
+gem 'guard'
+gem 'guard-rspec'
+gem 'rspec'
+gem 'simplecov'
+gem 'shoulda'
+gem 'rr'
+
+# .simplecov
+SimpleCov.start do
+  add_filter '/spec/'
 end
 
-require "test/unit"
-require "shoulda"
-require "rr"
+# spec/spec_helper.rb
+require 'rubygems'
+require 'simplecov'
+require 'simplecov-gem-adapter'
+require 'shoulda'
+require 'rr'
+require 'jekyll-import'
 
-unless defined?(Test::Unit::AssertionFailedError)
-  require "active_support"
-  class Test::Unit::AssertionFailedError < ActiveSupport::TestCase::Assertion
-  end
+SimpleCov.start('gem')
+
+RSpec.configure do |config|
+  config.color = true
+  config.formatter = :documentation
 end
 
-require File.expand_path("../lib/jekyll-import.rb", __dir__)
-include JekyllImport
+# spec/lib/jekyll_import_spec.rb
+require 'spec_helper'
 
-# Send STDERR into the void to suppress program output messages
-# STDERR.reopen(test(?e, '/dev/null') ? '/dev/null' : 'NUL:')
-
-class Test::Unit::TestCase
-  def dest_dir(*subdirs)
-    File.join(__dir__, "dest", *subdirs)
-  end
-
-  def source_dir(*subdirs)
-    File.join(__dir__, "source", *subdirs)
-  end
-
-  def clear_dest
-    FileUtils.rm_rf(dest_dir)
-  end
-
-  def capture_stdout
-    $old_stdout = $stdout
-    $stdout = StringIO.new
-    yield
-    $stdout.rewind
-    return $stdout.string
-  ensure
-    $stdout = $old_stdout
-  end
-
-  def capture_output(level = :debug)
-    buffer = StringIO.new
-    Jekyll.logger = Logger.new(buffer)
-    Jekyll.logger.log_level = level
-    yield
-    buffer.rewind
-    buffer.string.to_s
-  ensure
-    Jekyll.logger = Logger.new(StringIO.new, :error)
-  end
-end
+RSpec.describe JekyllImport do
+  describe '#dest_dir' do
+    it 'returns the path to the destination directory' do
+      expect(described_class.dest_dir).to eq(File
