9d88f10e5f92a0d63a8d7c7c0a68234cf33c53cb
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:43 +0300

    improve

--- test/test_dotclear_importer.rb
+++ test/test_dotclear_importer.rb
@@ -2,6 +2,7 @@
 
 require "helper"
 require "tempfile"
+require "fileutils"
 
 Importers::Dotclear.require_deps
 
@@ -10,47 +11,10 @@ class TestDotclearImporter < Test::Unit::TestCase
     Importers::Dotclear
   end
 
-  context "Importing with valid export file" do
-    setup do
-      orig_pwd = Dir.pwd
-      @export_file = File.join(orig_pwd, "test/mocks/dotclear.txt")
-
-      Dir.mktmpdir do |tmpdir|
-        Dir.chdir(tmpdir) do
-          @asset_file_path = "MiUser/250px-MonaLisaGraffiti.JPG"
-          @asset_src_path  = File.join("media_dir", @asset_file_path)
-
-          FileUtils.mkdir_p("media_dir/MiUser")
-          File.binwrite(@asset_src_path, "Hello")
-
-          @output = capture_output { described_class.run("datafile" => @export_file, "mediafolder" => "media_dir") }
-          @post_path = "_drafts/2017-03-25-welcome-to-dotclear.html"
-          @contents = File.read(@post_path)
-        end
-      end
-    end
-
-    should "log export file path" do
-      assert_includes @output, "Export File: #{@export_file}"
-    end
-
-    should "create post files with front matter and *adjusted* HTML content" do
-      assert_includes @output, "Creating: #{@post_path}"
-      assert_includes @contents, "---\nlayout: post\ntitle: Welcome to Dotclear!\n"
-      assert_includes @contents, "tags:\n- Indiana\n"
-      assert_includes @contents, "---\n\n<p style=\"color: blue\">This is your first entry."
-      assert_includes @contents, "\n\n<a href=\"/assets/dotclear/MiUser/250px-MonaLisaGraffiti.JPG>\">"
-    end
-
-    context "with media files" do
-      should "log copied path and destination path" do
-        assert_includes @output, "Copying: #{@asset_src_path}"
-        assert_includes @output, "To: #{File.join("assets/dotclear", @asset_file_path)}"
-      end
-
-      should "log missing media file path" do
-        assert_includes @output, "Not found: media_dir/MiUser/743px-Laurentius_de_Voltolina_001.jpg"
-      end
-    end
-  end
-end
+  def setup
+    @export_file = File.join(File.dirname(__FILE__), "mocks", "dotclear.txt")
+    @tmpdir = Dir.mktmpdir("dotclear_test_#{Time.now.strftime('%Y-%m-%d-%H%M%S')}")
+    Dir.chdir(@tmpdir) do
+      create_test_files
+      @output = capture_output { described_class.run("datafile" => @export_file, "mediafolder" => "media_dir") }
+      @post_path = "_drafts/2
