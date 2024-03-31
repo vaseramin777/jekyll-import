e9c66b21069a2ff499f4e7888a333416cacb1964
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:13 +0300

    improve

--- docs/_plugins/netlify_helper.rb
+++ docs/_plugins/netlify_helper.rb
@@ -1,18 +1,13 @@
 # frozen_string_literal: true
 
-# The Liquid templates for this site currently renders absolute URLs to various subpages, which
-# interferes with Netlify's "Deploy Preview" for pull requests containing changes to the docs-site.
-#
-# Therefore, override `{{ site.url }}` with the Netlify Deploy Preview URL for seamless navigation
-# during review.
-#
-# ref: https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
-#
-# DELETE THIS PLUGIN once the templates start rendering relative URLs to the subpages.
+# Override `site.url` with the Netlify Deploy Preview URL for seamless navigation during review.
+# Delete this plugin once the templates start rendering relative URLs to the subpages.
 
-return unless ENV["NETLIFY"]
-return if ENV["DEPLOY_PRIME_URL"].to_s.empty?
+return unless ENV.key?("NETLIFY")
+return if ENV.fetch("DEPLOY_PRIME_URL", "").empty?
 
-Jekyll::Hooks.register :site, :after_init do |site|
+require "jekyll"
+
+Jekyll::Hooks.register(:site, :after_init) do |site|
   site.config["url"] = ENV["DEPLOY_PRIME_URL"]
 end
