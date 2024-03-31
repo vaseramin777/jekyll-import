d54eb97a796d4e6c79c3d56948ef0ae956e27b5e
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-03-31 18:37:20 +0300

    improve

--- lib/jekyll-import/importers/drupal6.rb
+++ lib/jekyll-import/importers/drupal6.rb
@@ -8,11 +8,12 @@ module JekyllImport
       include DrupalCommon
       extend DrupalCommon::ClassMethods
 
-      def self.build_query(prefix, types, _engine)
+      # Builds a SQL query to fetch posts from Drupal 6 database
+      def self.build_query(prefix, types, engine)
         types = types.join("' OR n.type = '")
         types = "n.type = '#{types}'"
 
-        query = <<SQL
+        query = <<~SQL
                 SELECT n.nid,
                        n.title,
                        nr.body,
@@ -21,24 +22,30 @@ module JekyllImport
                        n.status,
                        ua.dst AS alias,
                        n.type,
-                       GROUP_CONCAT( td.name SEPARATOR '|' ) AS 'tags'
-                FROM #{prefix}node_revisions AS nr, url_alias AS ua,
-                     #{prefix}node AS n
-                     LEFT OUTER JOIN #{prefix}term_node AS tn ON tn.nid = n.nid
-                     LEFT OUTER JOIN #{prefix}term_data AS td ON tn.tid = td.tid
+                       GROUP_CONCAT( td.name SEPARATOR '|' ) AS tags
+                FROM #{prefix}node_revisions AS nr
+                JOIN #{prefix}node AS n ON n.nid = nr.nid AND n.vid = nr.vid
+                JOIN url_alias AS ua ON ua.src = CONCAT( 'node/', n.nid)
+                LEFT JOIN #{prefix}term_node AS tn ON tn.nid = n.nid
+                LEFT JOIN #{prefix}term_data AS td ON tn.tid = td.tid
                 WHERE (#{types})
-                  AND n.vid = nr.vid
-                  AND  ua.src = CONCAT( 'node/', n.nid)
                 GROUP BY n.nid, ua.dst
-SQL
+                SQL
 
-        query
+        begin
+          engine.query(query)
+        rescue StandardError => e
+          puts "Invalid SQL query: #{e.message}"
+          raise
+        end
       end
 
+      # Builds a SQL query to fetch aliases from Drupal 6 database
       def self.aliases_query(prefix)
         "SELECT src AS source, dst AS alias FROM #{prefix}url_alias WHERE src = ?"
       end
 
+      # Builds post data from SQL query result
       def self.post_data(sql_post_data)
         content = sql_post_data[:body].to_s
         summary = sql_post_data[:teaser].to_s
@@ -49,10 +56,4 @@ SQL
           "categories" => tags.split("|").uniq,
         }
 
-        data["permalink"] = "/" + sql_post_data[:alias] if sql_post_data[:alias]
-
-        [data, content]
-      end
-    end
-  end
-end
+        data["permalink"] = "/" + sql_post_data
